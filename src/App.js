3980d497f8bba86d4185511b224db18bd3cdb227
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-01-31 10:51:16 -0800

    render the to-dos

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       todos: [
         { description: 'Walk the cat', isCompited: true },
         { description: 'Throw the dishes away', isCompleted: false },
-        { description: 'Buy new dishes', isCopmleted: false }  
+        { description: 'Buy new dishes', isCopmleted: false }
       ]
     };
   }
@@ -19,8 +19,16 @@ class App extends Component {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo, index) =>
+            <ToDo key={ index } />
+          )}
+          //We use an ES6 arrow function for convenience,//
+          //and we enclose the call to .map() inside of curly braces,
+          //which tells JSX to evaluate the code between the braces as JavaScript.
+          //We also specify a key attribute on ToDo, and we assign it the index.
+          //For React to operate properly, each child of an array or iterator
+          // needs to have a key with a unique value.
+          // The key gives React a reliable way of distinguishing between array items.
         </ul>
       </div>
     );
