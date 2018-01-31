bdd22a3a38bd9d86095f65679c2e19cf8076deb1
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-01-31 11:34:43 -0800

    checkpoint_7_React_to_do_State_and_Props_complited

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     super(props);
     this.state = {
       todos: [
-        { description: 'Walk the cat', isCompited: true },
+        { description: 'Walk the cat', isCompleted: true },
         { description: 'Throw the dishes away', isCompleted: false },
-        { description: 'Buy new dishes', isCopmleted: false }
+        { description: 'Buy new dishes', isCompleted: false }
       ]
     };
   }
@@ -20,15 +20,17 @@ class App extends Component {
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } />
+            <ToDo key={ index }  description={ todo.description } isCompleted={ todo.isCompleted }/>
+            // in the ToDo component, we can now access those props on this.props object
           )}
-          //We use an ES6 arrow function for convenience,//
+          {/*We use an ES6 arrow function for convenience,//
           //and we enclose the call to .map() inside of curly braces,
           //which tells JSX to evaluate the code between the braces as JavaScript.
           //We also specify a key attribute on ToDo, and we assign it the index.
           //For React to operate properly, each child of an array or iterator
           // needs to have a key with a unique value.
           // The key gives React a reliable way of distinguishing between array items.
+          */}
         </ul>
       </div>
     );
