26c89557912973ff2792f4c629aa534ac31ed457
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-01-30 17:51:18 -0800

    Initial status defined

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompited: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCopmleted: false }  
+      ]
+    };
+  }
+
+
   render() {
     return (
       <div className="App">
